144e1a7478bb83f78c625758ff3c17a63d8f204c
docguerrammf <39508048-docguerrammf@users.noreply.replit.com>
2025-02-10 09:39:24 +0000

    Agent query: ¿Ahora se muestra correctamente la imagen de carga con los doctores en tonos verde/azulados?
    Update loading screen image
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/b77f2f3d-e000-4fde-ad3e-d2e11540aac7/e4560235-f245-4c2a-bf1d-7ff3694df96e.jpg

--- client/src/components/LoadingScreen.tsx
+++ client/src/components/LoadingScreen.tsx
@@ -5,7 +5,7 @@ export default function LoadingScreen() {
     <div className="fixed inset-0 bg-white">
       <div className="relative w-full h-full">
         <img
-          src="/a-dimly-lit-scene-with-two-doctors-and-a_hkwGmN5yQT6rqriux7-8jQ__SK5iPjDTIWGAz3rz3IASw.png"
+          src="/loading.png"
           alt="ObsteriX Legend Loading"
           className="w-full h-full object-cover"
         />
